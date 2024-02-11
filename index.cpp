d977d6d2a77f9bec91b6454e802030baf192029d
Kenneth <attinger.ken@gmail.com>
2024-02-10 20:23:19 -0600

    added for loop to draw cards

--- index.cpp
+++ index.cpp
@@ -6,8 +6,20 @@
 int main{
 
 
+for(i < 10; i++)
+{
+	cout << "Push enter to draw question card..." << endl;
+	pause;
 
+	cout << question << endl;
+	cout << "Press enter to draw answer card..." << endl;
+	pause;
 
+	cout << answer << endl;
+	cout << "Press enter to play again" << endl;
+	pause;
+	
+}
 
 
 
